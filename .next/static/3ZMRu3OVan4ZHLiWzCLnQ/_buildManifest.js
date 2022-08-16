f60b9a381b271c7dac43ed47f52fad7eec01c8bd
Wei Chen <wchen8878@gmail.com>
2022-08-16 00:18:33 -0400

    change to next

---
+++ .next/static/3ZMRu3OVan4ZHLiWzCLnQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6e082e0a2236eb4d.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
