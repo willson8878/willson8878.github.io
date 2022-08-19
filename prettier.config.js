753f7beda5b93dd32397c25e88a836e2cf3b3f1e
Wei Chen <wchen8878@gmail.com>
2022-08-19 00:24:33 -0400

    next.js setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options = {
+    arrowParents: 'avoid',
+    singleQuote:true,
+    bracketSpacing:true,
+    endOfLine:'lf',
+    tabwidth:4,
+    trailingComma:'none'
+}
+
+module.exports = options
