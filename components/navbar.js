508c5345032b5dc7a6507889e6bce41b33d258aa
Wei Chen <wchen8878@gmail.com>
2022-08-29 10:44:36 -0400

    Updated Nav Bar

---
+++ components/navbar.js
@@ -0,0 +1,90 @@
+import Logo from './logo'
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { IoLogoGithub } from 'react-icons/io5'
+
+const LinkItem = ({ href, path, children }) => {
+  const active = path === href;
+  const inactiveColor = useColorModeValue('gray200', 'whiteApha.900')
+  return (
+    <NextLink href={href}>
+      <Link p={2} bg={active ? 'glassTeal' : undefined} color={active ? '#202023' : inactiveColor}>
+        {children}
+      </Link>
+    </NextLink>
+  )
+}
+
+const NavBar = props => {
+  const { path } = props;
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#202023')}
+      style={{ backdropFilter: 'blur(10px)' }}
+      zIndex={1}
+      {...props}
+    >
+      <Container display="flex" p={2} maxW="container.md" wrap="warp" align="center" justify="space-between">
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            <Logo />
+          </Heading>
+        </Flex>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ hase: 'full', md: 'auto' }}
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, nmd: 0 }}
+        >
+          <LinkItem href="/works" path={path}>Work</LinkItem>
+          <LinkItem href="/posts" path={path}>Posts</LinkItem>
+          <IoLogoGithub />
+          <LinkItem href="https://github.com/willson8878/willson8878.github.io" path={path}>
+            
+            Source
+          </LinkItem>
+        </Stack>
+        <Box flex={1} align="right">
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu>
+              <MenuButton as={IconButton} icon={<HamburgerIcon />} variant="outline" arial-label="Options" />
+              <MenuList>
+                <NextLink href="/">
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href="/works">
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/posts">
+                  <MenuItem as={Link}>Posts</MenuItem>
+                </NextLink>
+                <MenuItem as={Link} href="https://github.com/willson8878/willson8878.github.io">View Source</MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  )
+}
+
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import { Container,Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
     return (
-        <div>Hello</div>
+      <Container>
+        <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+          Hello, I&apos;m a developer based in USA!
+        </Box>
+        <Box display={{md: 'flex'}}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Will (Wei) Chen
+            </Heading>
+            <p>Developer(Front Pages/small App)</p>
+          </Box>
+        </Box>
+      </Container>
     )
 }
 
